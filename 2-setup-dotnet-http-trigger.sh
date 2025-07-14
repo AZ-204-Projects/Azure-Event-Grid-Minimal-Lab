a5db612f9bcd94c2fa212f15018e16ee0bc18ffd
Steve LeVesconte <SteveLeVesconte@Gmail.com>
2025-07-14 09:22:48 -0700

    deleted .NET project change script to use .NET 8.0 ran script again azure function works as enpoint that returns 200 OK and text

--- 2-setup-dotnet-http-trigger.sh
+++ 2-setup-dotnet-http-trigger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-func init EventGridFunctionProj --worker-runtime dotnet
+func init EventGridFunctionProj --worker-runtime dotnet --target-framework net8.0
 cd EventGridFunctionProj
 func new --name EventPublisherFunction --template "HTTP trigger"
